912e4dcc0d1399ce68322c8ccad807b4081471dd
Timur Tuleuov <timmmur123@gmail.com>
2024-01-18 00:38:31 +0600

    fix: logo && fix: FAQ section && fix: style

--- src/app/component/faq-section/faq-section.component.ts
+++ src/app/component/faq-section/faq-section.component.ts
@@ -16,4 +16,19 @@ export class FaqSectionComponent implements OnInit{
     console.log(this.isNight)
   })
   }
+
+  accordionItems = [
+    { id: 0, isOpen: false },
+    { id: 1, isOpen: false },
+    { id: 2, isOpen: false },
+    { id: 3, isOpen: false },
+    // Добавьте другие блоки при необходимости
+  ];
+
+  toggleAccordion(itemId: number) {
+    const item = this.accordionItems.find(i => i.id === itemId);
+    if (item) {
+      item.isOpen = !item.isOpen;
+    }
+  }
 }
