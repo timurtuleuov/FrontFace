258fd0a8d40a81eba1e5b705af89b3b10468cb9c
Timur Tuleuov <timmmur123@gmail.com>
2024-01-22 00:44:15 +0600

    add: login component && new: ngMask library && TODO: fix problem with login HTML

---
+++ src/app/component/sign-in/sign-in.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NightmodeService } from 'src/app/service/nightmode.service';
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.css'
+})
+export class SignInComponent implements OnInit{ 
+  public isNight: boolean = false
+  
+  constructor(private nightMode: NightmodeService){}
+  ngOnInit(): void {
+  this.nightMode.value$.subscribe((newValue) => {
+    this.isNight = newValue
+    console.log(this.isNight)
+  })
+  }
+}
