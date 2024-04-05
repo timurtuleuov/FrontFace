7cb13bb2e995b4cdd6e99a22483c704e4e62cddb
Timur Tuleuov <timmmur123@gmail.com>
2024-04-05 17:23:48 +0500

    feat: loader

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './component/screens/home/home.component';
 import { AuthComponent } from './component/auth/auth.component';
 import { SignInComponent } from './component/sign-in/sign-in.component';
 import { FileLoadComponent } from './component/file-load/file-load.component';
+import { ResultPresentComponent } from './component/result-present/result-present.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'auth', component: AuthComponent},
   { path: 'sign-in', component: SignInComponent},
-  { path: 'load-file', component: FileLoadComponent}
+  { path: 'load-file', component: FileLoadComponent},
+  { path: 'present-result', component: ResultPresentComponent}
 ];
 
 @NgModule({
